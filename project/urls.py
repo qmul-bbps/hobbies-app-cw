bc0dacebfc3ec36e640e7ea91572c789c26ed9bb
Jake Coombs <57802613+jakecoombs@users.noreply.github.com>
2021-11-15 10:29:27 +0000

    Set up using django-ex repo

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""hobbies URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from welcome.views import index, health
 
 urlpatterns = [
+    path('', index, name='home'),
+    path('health/', health),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hobbies project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hobbies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
